b6288d1b33a3a9cc3935ef7e828eda60818f02e5
Daniel Norris <dnorris443@gmail.com>
2023-02-12 12:57:52 +0000

    feat(forge16): add guaranteed catch requirement

--- forge16/src/main/java/com/envyful/specs/forge/EnvySpecsForge.java
+++ forge16/src/main/java/com/envyful/specs/forge/EnvySpecsForge.java
@@ -1,9 +1,9 @@
 package com.envyful.specs.forge;
 
+import com.envyful.specs.forge.listener.GuaranteedCatchListener;
 import com.envyful.specs.forge.listener.UncatchableListener;
 import com.envyful.specs.forge.listener.UndeletableListener;
 import com.envyful.specs.forge.spec.*;
-import com.pixelmonmod.api.Flags;
 import com.pixelmonmod.api.pokemon.PokemonSpecificationProxy;
 import com.pixelmonmod.pixelmon.Pixelmon;
 import net.minecraftforge.fml.common.Mod;
@@ -14,6 +14,7 @@ public class EnvySpecsForge {
     public EnvySpecsForge() {
         Pixelmon.EVENT_BUS.register(new UndeletableListener());
         Pixelmon.EVENT_BUS.register(new UncatchableListener());
+        Pixelmon.EVENT_BUS.register(new GuaranteedCatchListener());
 
         PokemonSpecificationProxy.register(new AmIBossRequirement());
         PokemonSpecificationProxy.register(new MaxIVsWithMinPercentRequirement());
@@ -21,5 +22,6 @@ public class EnvySpecsForge {
         PokemonSpecificationProxy.register(new NumIVsRequirement());
         PokemonSpecificationProxy.register(new UndeletableRequirement());
         PokemonSpecificationProxy.register(new UncatchableRequirement());
+        PokemonSpecificationProxy.register(new GuaranteedCatchRequirement());
     }
 }
--- forge16/src/main/java/com/envyful/specs/forge/Flags.java
+++ forge16/src/main/java/com/envyful/specs/forge/Flags.java
@@ -3,5 +3,6 @@ package com.envyful.specs.forge;
 public class Flags {
 
     public static final String CANNOT_RELEASE = "cannot_release";
+    public static final String GUARANTEED_CATCH = "guaranteed_catch";
 
 }
